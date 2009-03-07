7be95f44e049229355e31de896021bd7a9c2caf6
Low Chin Chau <chinclow@gmail.com>
2009-03-08 01:53:24 +0800

    drop haml drop jquery (optional) fix .gitignore

--- baseline.rb
+++ baseline.rb
@@ -3,6 +3,7 @@ file '.gitignore', <<-GITIGNORE
 .DS_Store
 log/*.log
 tmp/**/*
+tmp/*
 TAGS
 *~
 .#*
@@ -30,40 +31,40 @@ run 'rm public/index.html'
 run 'rm public/favicon.ico'
 run 'rm public/robots.txt'
 
-if yes?("Use jQuery?")
-  load_template( 'http://github.com/cclow/rails-templates/raw/master/use_jquery.rb')
-end
-
 run "cp config/database.yml config/example_database.yml"
 run "find . -type d -empty | xargs -I xxx touch xxx/.gitignore"
 
-# gem 'cucumber'
-# gem 'webrat'
-
-inside('config/environments') do
-  run "cat >> test.rb <<-END
-
-# gems for testing
-config.gem 'mocha'
-config.gem 'faker'
-config.gem 'thoughtbot-shoulda', :lib => 'shoulda'
-config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl'
-END"
+inside("config/environments") do
+  run 'cat >> test.rb <<-END_TEST_GEMS
+config.gem "cucumber",
+  :version => ">=0.1.16"
+config.gem "webrat",
+  :version => ">=0.4.2"
+config.gem "mocha",
+  :version => ">=0.9.5"
+config.gem "faker",
+  :version => ">=0.3.1"
+config.gem "thoughtbot-shoulda",
+  :lib => "shoulda",
+  :source => "http://gems.github.com",
+  :version => ">=2.10.0"
+config.gem "thoughtbot-factory_girl",
+  :lib => "factory_girl",
+  :source => "http://gems.github.com",
+  :version => ">=1.2.0"
+END_TEST_GEMS'
 end
-
-# gem 'thoughtbot-factory_girl', :lib => 'factory_girl'
-# gem 'thoughtbot-shoulda', :lib => 'shoulda'
-# gem 'faker'
-# gem 'mocha'
 # 
-gem "sqlite3-ruby", :lib => "sqlite3"
-gem 'authlogic'
-gem 'mislav-will_paginate', :lib => 'will_paginate'
-
-if yes?("Use Haml?")
-  gem 'haml'
-  run 'haml --rails .'
-end
+gem "dbd-sqlite3",
+  :lib => "sqlite3",
+  :source => "http://gems.github.com",
+  :version => ">=1.2.4"
+gem "authlogic",
+  :version => ">=1.4.3"
+gem 'mislav-will_paginate',
+  :lib => 'will_paginate'
+  :source => "http://gems.github.com",
+  :version => ">=2.3.7"
 
 git :add => '.'
 git :commit => '-m "Initial commit"'
