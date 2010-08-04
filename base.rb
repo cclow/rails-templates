be2eb3d79cab5a679bd4226375971eb3ad7676e8
Low Chin Chau <cclow@ufinity.com>
2010-08-05 00:14:27 +0800

    updated for Rails3 RC

--- base.rb
+++ base.rb
@@ -5,6 +5,10 @@ run 'rm public/images/rails.png'
 
 git :init
 run %Q|cat >> '.gitignore' <<-GITIGNORE
+.bundle
+db/*.sqlite3
+log/*.log
+tmp/**/*
 .DS_Store
 TAGS
 *~
@@ -20,12 +24,13 @@ public/stylesheets/compiled/*
 GITIGNORE|
 
 gem 'haml', '>=3.0.13'
-gem 'compass', '>=0.10.2'
+# gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'will_paginate', :branch => 'rails3', :git => 'git://github.com/cclow/will_paginate.git'
 
 gem 'faker', :group => [:test, :development]
 gem 'factory_girl_rails', :group => [:test, :development]
 gem 'capybara', :group => :test
-gem 'rspec-rails', '>=2.0.0.beta.14', :group => :test
+gem 'rspec-rails', '>=2.0.0.beta.19', :group => :test
 gem 'cucumber-rails', :group => :test
 gem 'autotest', :group => :test
 gem 'autotest-rails', :group => :test
@@ -33,36 +38,28 @@ gem 'shoulda', :group => :test
 gem 'rails3-generators', :group => :development
 gem "awesome_print", :group => :development
 
-run 'bundle install' if yes?('install bundle?')
+run 'bundle install'
 
 generate 'rspec:install'
 generate 'cucumber:install', "--rspec", "--capybara"
 
 run 'cp config/database.yml config/database-sample.yml'
 
-inject_into_file 'spec/spec_helper.rb', %Q{\# insert shoulda matchers\nrequire 'shoulda'\n}, :after => %Q{require 'rspec/rails'\n}
+inject_into_file 'spec/spec_helper.rb', %Q{\# insert shoulda matchers\nrequire 'shoulda'\n},
+  :after => %Q{require 'rspec/rails'\n}
 inject_into_file 'spec/spec_helper.rb',
   %Q{  include Shoulda::ActiveRecord::Matchers\n  include Shoulda::ActionController::Matchers\n\n},
   :after => %Q{Rspec.configure do |config|\n}
 
-run 'compass init rails . --using blueprint/semantic --quiet --sass-dir app/stylesheets --css-dir public/stylesheets/compiled'
-
 if yes?('Download JQuery?')
-  run 'curl -L http://code.jquery.com/jquery-1.4.2.min.js > public/javascripts/jquery-1.4.2.min.js'
+  run 'curl -L http://code.jquery.com/jquery-1.4.2.min.js > public/javascripts/jquery.js'
   run 'curl -L http://github.com/rails/jquery-ujs/raw/master/src/rails.js > public/javascripts/rails.js'
 else
   run 'cp ../rails-templates/archive/jquery/rails.js public/javascripts/rails.js'
-  run 'cp ../rails-templates/archive/jquery/jquery-1.4.2.min.js public/javascripts/jquery-1.4.2.min.js'
+  run 'cp ../rails-templates/archive/jquery/jquery-1.4.2.min.js public/javascripts/jquery.js'
 end
 
-initializer 'jquery.rb', <<-JQUERY
-module ActionView::Helpers::AssetTagHelper
-  remove_const :JAVASCRIPT_DEFAULT_SOURCES
-  JAVASCRIPT_DEFAULT_SOURCES = %w(jquery-1.4.2.min.js rails.js)
-
-  reset_javascript_include_default
-end
-JQUERY
+application %Q{config.action_view.javascript_expansions[:defaults] = %w(jquery rails)}
 
 initializer 'factory_girl.rb', <<-FACTORY_GIRL
 require File.join(Rails.root, 'factory_girl', 'factories') unless Rails.env == 'production'
