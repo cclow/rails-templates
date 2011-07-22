1d109ac3e5af4e24d98bb4b2bbb1345188062b5f
Low Chin Chau <chinclow@gmail.com>
2011-07-22 22:33:44 +0800

    update for rails 3.1 and using Skeleton

--- archive/base/factories.rb
+++
@@ -1,4 +0,0 @@
-require "faker"
-require "factory_girl/syntax/sham"
-
-Dir.glob(File.join(File.dirname(__FILE__), "/factories/*.rb")).each { |f| require f }
--- base.rb
+++ base.rb
@@ -1,3 +1,6 @@
+# run with
+# rails new project -T --skip-bundle -m rails-templates/base.rb
+#
 require 'open-uri'
 
 @archive ||= File.join(File.dirname(__FILE__), 'archive')
@@ -10,102 +13,59 @@ def archive_copy(archive, from, to)
   end
 end
 
-run 'rm README'
+git :init
+
+run 'echo "rvm use `rvm current`" | sed "s/@.*//" > .rvmrc'
+run 'rvm rvmrc trust'
+
 run 'rm doc/README_FOR_APP'
 run 'rm public/index.html'
-run 'rm public/images/rails.png'
+run 'rm app/assets/images/rails.png'
 
-git :init
-archive_copy(@archive, 'base/gitignore', '.gitignore')
-
-gem 'haml', '>=3.0.17'
-gem 'will_paginate', ">=3.0.pre2"
 gem "simple_form"
-gem 'escape_utils'
-gem 'hpricot'
-gem 'ruby_parser'
-gem 'devise'
-gem 'cancan'
+gem "haml-rails"
 
 gem 'faker', :group => [:test, :development]
 gem 'factory_girl_rails', :group => [:test, :development]
 gem 'rspec-rails', :group => [ :test, :development ]
 gem 'capybara', :group => :test
-gem 'cucumber-rails', :group => :test
 gem 'autotest', :group => :test
 gem 'autotest-rails', :group => :test
-gem 'shoulda', :group => :test
+gem 'shoulda-matchers', :group => :test
 gem 'launchy', :group => :test
 gem 'email_spec', :group => :test
 gem 'rails3-generators', :group => :development
 gem "awesome_print", :group => :development
-gem "haml-rails", :group => :development
-gem 'jquery-rails', :group => :development
 
-run 'bundle install'
+run 'bundle install --path vendor && bundle package && echo "vendor/ruby" >> .gitignore'
 
-generate 'cancan:ability'
 generate 'simple_form:install'
 generate 'rspec:install'
-generate 'cucumber:install', "--rspec", "--capybara"
 
 run 'cp config/database.yml config/database-sample.yml'
-
-archive_copy(@archive, '/base/escape_utils.rb', 'config/initializers/escape_utils.rb')
-
-run 'mkdir -p spec/support'
-archive_copy(@archive, '/base/shoulda.rb', 'spec/support/shoulda.rb')
-
-initializer 'factory_girl.rb', <<-FACTORY_GIRL
-require File.join(Rails.root, 'factory_girl', 'factories') unless Rails.env == 'production'
-FACTORY_GIRL
-
-run 'mkdir -p factory_girl/factories'
-archive_copy(@archive, 'base/factories.rb', 'factory_girl/factories.rb')
-
-generate 'haml:install'
-
-run 'mkdir -p app/sass'
-run 'mkdir -p public/stylesheets/compiled'
-run 'touch app/sass/screen.scss'
-run 'touch app/sass/print.scss'
-run 'touch app/sass/ie.scss'
-
-initializer 'sass.rb', <<-SASS
-Sass::Plugin.options[:style] = :compact
-Sass::Plugin.options[:template_location] = { 'app/sass' => 'public/stylesheets/compiled' }
-SASS
-
-generate 'jquery:install', '--ui'
-
-gsub_file "config/application.rb", /^\s+# JavaScript.*\n/, ""
-gsub_file "config/application.rb", /^\s+config\.action_view\.javascript.*\n/, ""
-application "config.action_view.javascript_expansions[:defaults] = %w(jquery.min jquery-ui.min rails)"
+run 'echo "config/database.yml" >> .gitignore'
 
 # add default layout and home page
 archive_copy(@archive, 'base/layout_helper.rb', 'app/helpers/layout_helper.rb')
-archive_copy(@archive, 'base/application.html.haml', 'app/views/layouts/application.html.haml')
 run 'rm app/views/layouts/application.html.erb'
+archive_copy(@archive, 'base/application.html.haml', 'app/views/layouts/application.html.haml')
+archive_copy(@archive, 'base/images/favicon.ico', 'app/assets/images/favicon.ico')
+archive_copy(@archive, 'base/images/apple-touch-icon.png', 'app/assets/images/apple-touch-icon.png')
+archive_copy(@archive, 'base/images/apple-touch-icon-72x72.png', 'app/assets/images/apple-touch-icon-72x72.png')
+archive_copy(@archive, 'base/images/apple-touch-icon-114x114.png', 'app/assets/images/apple-touch-icon-114x114.png')
+archive_copy(@archive, 'base/stylesheets/base.css', 'app/assets/stylesheets/base.css')
+archive_copy(@archive, 'base/stylesheets/skeleton.css', 'app/assets/stylesheets/skeleton.css')
+archive_copy(@archive, 'base/stylesheets/layout.css', 'app/assets/stylesheets/layout.css')
+archive_copy(@archive, 'base/javascripts/tabs.js', 'app/assets/javascripts/tabs.js')
 
 initializer 'rails3_generators.rb', <<-RAILS3_GEN
-Rails.application.class.configure do
-  config.generators do |g|
-    g.template_engine :haml
-    g.test_framework :rspec, :fixture => true, :views => false
-    g.fixture_replacement :factory_girl, :dir => 'factory_girl/factories'
-  end
+Rails.application.config.generators do |g|
+  g.test_framework :rspec, :fixture => true, :views => false
+  g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+  g.form_builder :simple_form
+  g.template_engine :haml
 end
 RAILS3_GEN
 
-apply File.join(File.dirname(__FILE__), 'use_auto_focus.rb')
-
-if yes?("Apply JQuery Pageless?")
-  apply File.join(File.dirname(__FILE__), 'use_pageless.rb')
-end
-
-if yes?("Apply JQuery Anytime Date Time Picker?")
-  apply File.join(File.dirname(__FILE__), 'use_anytime.rb')
-end
-
 git :add => "."
-git :commit => '-m "Rails 3 app with baseline template"'
+git :commit => '-m "Rails 3.1 app with baseline template"'
