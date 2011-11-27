e003b807ac65ac20e2c4ba9bf9921f45085e0a10
Low Chin Chau <chinclow@gmail.com>
2011-11-27 08:25:49 +0800

    fixed model_steps for cucumber fixed spork bootstrapping switch from bundle install --path vendor to using a separate gemset switch back to haml from slim simplify and other small fixes

--- base.rb
+++ base.rb
@@ -5,65 +5,92 @@ require File.join(File.dirname(__FILE__), 'archive_helper')
 
 git :init
 
-run 'echo "# rvm current environment : `rvm current`" > .rvmrc'
-run 'echo "rvm use `rvm current`" | sed "s/@.*//" > .rvmrc'
-run 'rvm rvmrc trust'
+# Need to do this before call the template since
+# doing a run "rvm ..." does not change the rvm gemset
+#
+# run "rvm gemset create #{app_name}"
+# run "rvm gemset use #{app_name}"
+# 
+ruby_gemset = `rvm current`.strip
 
-remove_file 'doc/README_FOR_APP'
-remove_file 'public/index.html'
-remove_file 'app/assets/images/rails.png'
+create_file ".rvmrc", "rvm use #{ruby_gemset}"
+
+# remove_file 'doc/README_FOR_APP'
+# remove_file 'public/index.html'
+# remove_file 'app/assets/images/rails.png'
 
 run 'cp config/database.yml config/database-sample.yml'
 append_file '.gitignore', "config/database.yml\n"
 
-gem 'simple_form'
-gem 'slim-rails'
+append_file 'Gemfile', <<-GEMFILE
+  gem 'simple_form'
+  gem 'haml-rails'
+  # gem 'slim-rails'
 
-gem 'faker', :group => [:test, :development]
-gem 'factory_girl_rails', :group => [:test, :development]
-gem 'rspec-rails', :group => [:test, :development ]
-gem 'cucumber-rails', :group => [:test, :development ]
-gem 'capybara', :group => [:test, :development]
-gem 'akephalos', :group => [:test, :development]
-gem 'jasmine', :group => [:test, :development ]
-gem 'launchy', :group => [:test, :development]
-gem 'database_cleaner', :group => [:test, :development]
-gem 'guard-rspec', :group => [:test, :development]
-gem 'guard-cucumber', :group => [:test, :development]
-gem 'shoulda-matchers', :group => [:test, :development]
-gem 'rails3-generators', :group => :development
-gem 'awesome_print', :group => :development
-gem 'rb-fsevent', :require => false, :group => [:test, :development] if RUBY_PLATFORM =~ /darwin/i
+  group :development do
+    gem 'rails3-generators'
+    gem 'awesome_print'
+  end
+
+  group :test, :development do
+    gem 'faker'
+    gem 'factory_girl_rails'
+    gem 'rspec-rails'
+    gem 'shoulda-matchers'
+    gem 'capybara'
+    gem 'capybara-webkit'
+    gem 'launchy'
+    gem 'database_cleaner'
+    gem 'guard-bundler'
+    gem 'spork'
+    gem 'guard-spork'
+    gem 'guard-rspec'
+    gem 'rb-fsevent', :require => false if RUBY_PLATFORM =~ /darwin/i
+    gem 'growl' if RUBY_PLATFORM =~ /darwin/i
+  end
+GEMFILE
 
 append_file '.gitignore', "vendor/ruby\n"
-run 'bundle install --path vendor && bundle package'
+run 'bundle install'
 
 generate 'simple_form:install'
-generate 'cucumber:install'
 generate 'rspec:install'
 inside 'spec' do
   empty_directory 'routing'
   empty_directory 'support'
+  empty_directory 'requests'
 end
-run 'bundle exec jasmine init'
-remove_file 'lib/tasks/jasmine.rake'
-run 'bundle exec guard init rspec'
-run 'bundle exec guard init cucumber'
+
+run 'spork --bootstrap'
+inject_into_file 'spec/spec_helper.rb', :after => 'Spork.each_run do' do
+%Q{
+  load ::Rails.root.join('config','routes.rb')
+  Dir[::Rails.root.join('app','**','*.rb')].each {|f| load f}
+  Dir[::Rails.root.join('spec','support','**','*.rb')].each {|f| load f}
+  FactoryGirl.reload
+}
+end
+
+run 'guard init bundler'
+run 'guard init spork'
+run 'guard init rspec'
 
 # add default layout and home page
 archive_copy('base/layout_helper.rb', 'app/helpers/layout_helper.rb')
-archive_copy('base/application.html.slim', 'app/views/layouts/application.html.slim')
+archive_copy('base/application.html.haml', 'app/views/layouts/application.html.haml')
+# archive_copy('base/application.html.slim', 'app/views/layouts/application.html.slim')
 remove_file 'app/views/layouts/application.html.erb'
 
-initializer 'slim.rb', <<-SLIM
-Slim::Engine.set_default_options :pretty => true unless Rails.env == 'production'
-SLIM
-
+# initializer 'slim.rb', <<-SLIM
+# Slim::Engine.set_default_options :pretty => true unless Rails.env == 'production'
+# SLIM
+#
 initializer 'rails3_generators.rb', <<-RAILS3_GEN
 Rails.application.config.generators do |g|
   g.test_framework :rspec, :fixture => true, :views => false
   g.fixture_replacement :factory_girl, :dir => 'spec/factories'
-  g.template_engine :slim
+  g.template_engine :haml
+  # g.template_engine :slim
 end
 RAILS3_GEN
 
