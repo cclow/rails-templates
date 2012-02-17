d83a203744ded67f725a7ecae420128e81d66ba0
Low Chin Chau <chinclow@gmail.com>
2012-02-17 15:37:00 +0800

    Add Devise test helper

--- use_devise.rb
+++ use_devise.rb
@@ -4,3 +4,11 @@ gem "devise"
 run "bundle install"
 
 generate "devise:install"
+
+create_file "spec/support/devise.rb" do
+%q<
+RSpec.configure do |config|
+  config.include Devise::TestHelpers, :type => :controller
+end
+>
+end
